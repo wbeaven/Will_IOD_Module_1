9f5853d40b8fa442160f376a9105029470535317
Will <wtbeaven@gmail.com>
2025-02-05 23:16:13 +1100

    Finished lab work 3

---
+++ Lab Exercise 3/scripts.js
@@ -0,0 +1,9 @@
+function ChangeHeading1() {
+  document.getElementById("heading1").innerText =
+    document.getElementById("txtJob").value;
+  document.getElementById("column1").style.backgroundColor = "lightcoral";
+}
+function ChangeHeading2() {
+  document.getElementById("heading2").innerText = "World has been changed!";
+  document.getElementById("column2").style.backgroundColor = "lightblue";
+}
